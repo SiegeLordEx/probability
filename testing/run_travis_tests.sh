3ae7348e44368fb165897db8f8a7fe38b334ca47
Pavel Sountsov <siege@google.com>
2020-11-24 16:01:39 -0800

    Shard the tests

--- testing/run_travis_tests.sh
+++ testing/run_travis_tests.sh
@@ -23,6 +23,17 @@ set -u  # fail and exit on any undefined variable reference
 # Get the absolute path to the directory containing this script.
 DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
+# Make sure the environment variables are set.
+if [ -z "${SHARD}" ]; then
+  echo "SHARD is unset."
+  exit -1
+fi
+
+if [ -z "${NUM_SHARDS}" ]; then
+  echo "NUM_SHARDS is unset."
+  exit -1
+fi
+
 install_bazel() {
   # Install Bazel for tests. Based on instructions at
   # https://docs.bazel.build/versions/master/install-ubuntu.html#install-on-ubuntu
@@ -48,4 +59,30 @@ install_python_packages() {
 which bazel || install_bazel
 install_python_packages
 
-$DIR/run_tfp_test.sh //tensorflow_probability/...
+test_tags_to_skip="(gpu|requires-gpu-nvidia|notap|no-oss-ci|tfp_jax|tf2-broken|tf2-kokoro-broken)"
+
+# Given a test size (small, medium, large), a number of shards and a shard ID,
+# query and print a list of tests of the given size to run in the given shard.
+query_and_shard_tests_by_size() {
+  size=$1
+  bazel_query="attr(size, ${size}, tests(//tensorflow_probability/...)) \
+               except \
+               attr(tags, \"${test_tags_to_skip}\", \
+                    tests(//tensorflow_probability/...))"
+  bazel query ${bazel_query} \
+    | awk -v n=${NUM_SHARDS} -v s=${SHARD} 'NR%n == s'
+}
+
+# Generate a list of tests for this shard, consisting of a subset of tests of
+# each size (small, medium and large). By evenly splitting the various test
+# sizes across shards, we help ensure the shards have comparable runtimes.
+sharded_tests="$(query_and_shard_tests_by_size small)"
+sharded_tests="${sharded_tests} $(query_and_shard_tests_by_size medium)"
+sharded_tests="${sharded_tests} $(query_and_shard_tests_by_size large)"
+
+# Run tests using run_tfp_test.sh script. We append the following flags:
+#   --notest_keep_going -- stop running tests as soon as anything fails. This is
+#     to minimize load on Travis, where we share a limited number of concurrent
+#     jobs with a bunch of other TensorFlow projects.
+echo "${sharded_tests}" \
+  | xargs $DIR/run_tfp_test.sh --notest_keep_going
