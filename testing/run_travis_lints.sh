d91515c2c6bbcc8e2d26f478537b4e9ecefff271
Pavel Sountsov <siege@google.com>
2020-11-24 17:18:20 -0800

    Try to fix hashes 2

--- testing/run_travis_lints.sh
+++ testing/run_travis_lints.sh
@@ -18,11 +18,13 @@ set -v  # print commands as they are executed
 set -e  # fail and exit on any command erroring
 
 get_changed_py_files() {
-  # Need to fetch the base branch in case it is not master.
-  git diff \
-      --name-only \
-      --diff-filter=AM ${BEFORE_HASH}...${AFTER_HASH}\
-    | grep '^tensorflow_probability.*\.py$' || true
+  if [ $GITHUB_BASE_REF ]; then
+    git fetch origin ${GITHUB_BASE_REF} --depth=1
+    git diff \
+        --name-only \
+        --diff-filter=AM origin/${GITHUB_BASE_REF}...${GITHUB_SHA} \
+      | grep '^tensorflow_probability.*\.py$'
+  fi
 }
 
 pip install --quiet pylint
